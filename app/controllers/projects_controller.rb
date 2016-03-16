a8f687006d9521989dd75d0023684c84854ddd4b
globalavocado <globalavocado@gmail.com>
2016-03-16 23:33:25 +0000

    project description submittable through form and appears on project page, all tests passed

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,66 +1,66 @@
 class ProjectsController < ApplicationController
 
-	before_action :authenticate_user!, :except => [:index, :show]
+  before_action :authenticate_user!, :except => [:index, :show]
 
-	def index
-		@projects = Project.all
-	end
+  def index
+    @projects = Project.all
+  end
 
-	def new
-		authenticate_user!
-		@project = Project.new
-	end
+  def new
+    authenticate_user!
+    @project = Project.new
+  end
 
-	def create
-		@project = Project.new(project_params)
-		@project.user = current_user
-		if @project.save
-			redirect_to projects_path
-		else
-			render 'new'
-		end
-	end
+  def create
+    @project = Project.new(project_params)
+    @project.user = current_user
+      if @project.save
+        redirect_to projects_path
+      else
+        render 'new'
+    end
+  end
 
-	def project_params
-		params.require(:project).permit(:name)
-	end
+  def project_params
+    params.require(:project).permit(:name, :description)
+  end
 
-	def show
-		@project = Project.find(params[:id])
-	end
+  def show
+    @project = Project.find(params[:id])
+  end
 
-	def edit
-		@project = Project.find(params[:id])
-		if current_user.id != @project.user_id
-			flash[:notice] = "you cannot edit somebody else's project."
-			redirect_to projects_path
-		else
-			render 'edit'
-		end
-	end
+  def edit
+    @project = Project.find(params[:id])
+    if current_user.id != @project.user_id
+      flash[:notice] = "you cannot edit somebody else's project."
+      redirect_to projects_path
+    else
+      render 'edit'
+    end
+  end
 
-	def update
-		@project = Project.find(params[:id])
-			if current_user.id != @project.user_id
-			flash[:notice] = "you cannot edit somebody else's project."
-			redirect_to projects_path
-		else
-			@project.update(project_params)
-			flash[:notice] = 'Project edited successfully'
-			redirect_to projects_path
-		end
-	end
+  def update
+    @project = Project.find(params[:id])
+      if current_user.id != @project.user_id
+      flash[:notice] = "you cannot edit somebody else's project."
+      redirect_to projects_path
+    else
+      @project.update(project_params)
+      flash[:notice] = 'Project edited successfully'
+      redirect_to projects_path
+    end
+  end
 
-	def destroy
-		@project = Project.find(params[:id])
-		if current_user.id != @project.user_id
-			flash[:notice] = "you cannot delete somebody else's project."
-			redirect_to projects_path
-		else
-			@project.destroy
-			flash[:notice] = 'Project deleted successfully'
-			redirect_to projects_path
-		end
-	end
+  def destroy
+    @project = Project.find(params[:id])
+    if current_user.id != @project.user_id
+      flash[:notice] = "you cannot delete somebody else's project."
+      redirect_to projects_path
+    else
+      @project.destroy
+      flash[:notice] = 'Project deleted successfully'
+      redirect_to projects_path
+    end
+  end
 
 end
--- spec/features/projects_features_spec.rb
+++ spec/features/projects_features_spec.rb
@@ -1,149 +1,151 @@
 require 'rails_helper'
 
-	def signup_user (email, password)
-		visit '/' 
-		click_link 'sign up' 
-		fill_in 'Email', with: email
-		fill_in 'Password', with: password
-		fill_in 'Password confirmation', with: password
-		click_button 'Sign up' 
-	end
+  def signup_user (email, password)
+    visit '/' 
+    click_link 'sign up' 
+    fill_in 'Email', with: email
+    fill_in 'Password', with: password
+    fill_in 'Password confirmation', with: password
+    click_button 'Sign up' 
+  end
 
 feature 'projects:' do
 
-	context 'no projects have been added' do
-		scenario 'should display a prompt to add a project' do
-			visit '/projects'
-			expect(page).to have_content 'no projects yet!'
-			expect(page).to have_link 'add a project'
-		end
-	end
+  context 'no projects have been added' do
+    scenario 'should display a prompt to add a project' do
+      visit '/projects'
+      expect(page).to have_content 'no projects yet!'
+      expect(page).to have_link 'add a project'
+    end
+  end
 
-	context 'creating projects:' do
-		scenario 'prompts user to fill out a form, then displays the new project' do
-			signup_user('test@example.com', 'password123')
-			click_link 'add a project'
-			fill_in 'Name', with: 'Crowdfundingtestproject'
-			click_button 'create project'
-			expect(page).to have_content 'Crowdfundingtestproject'
-			expect(current_path).to eq '/projects'
-		end
-	end
+  context 'creating projects:' do
+    scenario 'prompts user to fill out a form, then displays the new project' do
+      signup_user('test@example.com', 'password123')
+      click_link 'add a project'
+      fill_in 'Name', with: 'Crowdfundingtestproject'
+      fill_in 'Description', with: 'this is the description'
+      click_button 'create project'
+      click_link 'Crowdfundingtestproject'
+      expect(page).to have_content 'Crowdfundingtestproject'
+      expect(page).to have_content 'this is the description'
+    end
+  end
 
-	context 'projects have been added:' do
-	  	 	before do
-		  	signup_user('test@example.com', 'password123')
-			click_link 'add a project'
-			fill_in 'Name', with: 'Crowdfundingtestproject'
-			click_button 'create project'
-		end
+  context 'projects have been added:' do
+    before do
+      signup_user('test@example.com', 'password123')
+      click_link 'add a project'
+      fill_in 'Name', with: 'Crowdfundingtestproject'
+      click_button 'create project'
+    end
 
-		scenario 'display projects' do
-			visit '/projects'
-			expect(page).to have_content('Crowdfundingtestproject')
-			expect(page).not_to have_content('no projects yet')
-		end
+    scenario 'display projects' do
+      visit '/projects'
+      expect(page).to have_content('Crowdfundingtestproject')
+      expect(page).not_to have_content('no projects yet')
+    end
 
-	end
+  end
 
-	context 'viewing projects:' do
-		before do
-			signup_user('test2@example.com', 'password678')
-			click_link 'add a project'
-			fill_in 'Name', with: "Crowdfundingtestproject"
-			click_button 'create project'
-		end
+  context 'viewing projects:' do
+    before do
+      signup_user('test2@example.com', 'password678')
+      click_link 'add a project'
+      fill_in 'Name', with: "Crowdfundingtestproject"
+      click_button 'create project'
+    end
 
-		scenario 'lets a user view a project page' do
-			visit '/projects'
-			click_link 'Crowdfundingtestproject'
-			expect(page).to have_content 'Crowdfundingtestproject'
-		end
+    scenario 'lets a user view a project page' do
+      visit '/projects'
+      click_link 'Crowdfundingtestproject'
+      expect(page).to have_content 'Crowdfundingtestproject'
+    end
 
-		scenario 'display user email to identify creator of project' do
-			click_link 'log out'
-			visit '/projects'
-			signup_user('test@example.com', 'password123')
-			click_link 'add a project'
-			fill_in 'Name', with: "Chris' fantastic flying Cola bottles"
-			click_button 'create project'
-			click_link "Chris' fantastic flying Cola bottles"
-			expect(page).to have_content('added by test@example.com')
-		end
+    scenario 'display user email to identify creator of project' do
+      click_link 'log out'
+      visit '/projects'
+      signup_user('test@example.com', 'password123')
+      click_link 'add a project'
+      fill_in 'Name', with: "Chris' fantastic flying Cola bottles"
+      click_button 'create project'
+      click_link "Chris' fantastic flying Cola bottles"
+      expect(page).to have_content('added by test@example.com')
+    end
 
-	end
+  end
 
-	context 'editing projects:' do
-	 	before do
-		  	signup_user('test@example.com', 'password123')
-			click_link 'add a project'
-			fill_in 'Name', with: 'Crowdfundingtestproject'
-			click_button 'create project'
-		end
+  context 'editing projects:' do
+    before do
+      signup_user('test@example.com', 'password123')
+      click_link 'add a project'
+      fill_in 'Name', with: 'Crowdfundingtestproject'
+      click_button 'create project'
+    end
 
-		scenario 'let a user edit a project' do
-			visit '/projects'
-			click_link 'edit Crowdfundingtestproject'
-			fill_in 'Name', with: 'The Fantastic Crowdfunding Project'
-			click_button 'update project'
-			expect(page).to have_content 'The Fantastic Crowdfunding Project'
-			expect(current_path).to eq '/projects'
-		end
+    scenario 'let a user edit a project' do
+      visit '/projects'
+      click_link 'edit Crowdfundingtestproject'
+      fill_in 'Name', with: 'The Fantastic Crowdfunding Project'
+      click_button 'update project'
+      expect(page).to have_content 'The Fantastic Crowdfunding Project'
+      expect(current_path).to eq '/projects'
+    end
 
-		scenario 'users can only edit their own projects' do
-			click_link 'log out'
-			signup_user('test2@example.com', 'password678')
-			visit '/projects'
-			click_link 'edit Crowdfundingtestproject'
-			expect(page).to have_content("you cannot edit somebody else's project.")
-		end
+    scenario 'users can only edit their own projects' do
+      click_link 'log out'
+      signup_user('test2@example.com', 'password678')
+      visit '/projects'
+      click_link 'edit Crowdfundingtestproject'
+      expect(page).to have_content("you cannot edit somebody else's project.")
+    end
 
-	end
+  end
 
-	context 'deleting projects:' do
-	  before do
-			signup_user('test@example.com', 'password123')
-			click_link 'add a project'
-			fill_in 'Name', with: 'Crowdfundingtestproject'
-			click_button 'create project'
-	  end
+  context 'deleting projects:' do
+    before do
+      signup_user('test@example.com', 'password123')
+      click_link 'add a project'
+      fill_in 'Name', with: 'Crowdfundingtestproject'
+      click_button 'create project'
+    end
 
-		scenario 'removes a project when a user clicks a delete link' do
-			visit '/projects'
-			click_link 'delete Crowdfundingtestproject'
-			expect(page).not_to have_content 'Crowdfundingtestproject'
-			expect(page).to have_content 'Project deleted successfully'
-		end
+    scenario 'removes a project when a user clicks a delete link' do
+      visit '/projects'
+      click_link 'delete Crowdfundingtestproject'
+      expect(page).not_to have_content 'Crowdfundingtestproject'
+      expect(page).to have_content 'Project deleted successfully'
+    end
 
-		scenario 'users can only delete their own projects' do
-			click_link 'log out'
-			signup_user('test2@example.com', 'password678')
-			visit '/projects'
-			click_link 'delete Crowdfundingtestproject'
-			expect(page).to have_content("you cannot delete somebody else's project.")
-		end
+    scenario 'users can only delete their own projects' do
+      click_link 'log out'
+      signup_user('test2@example.com', 'password678')
+      visit '/projects'
+      click_link 'delete Crowdfundingtestproject'
+      expect(page).to have_content("you cannot delete somebody else's project.")
+    end
 
-		scenario 'logged out users will not see links to edit or delete projects' do
-		  click_link 'log out'
-		  visit '/projects'
-		  expect(page).not_to have_link('edit Crowdfundingtestproject')
-		  expect(page).not_to have_link('delete Crowdfundingtestproject')
-		end
+    scenario 'logged out users will not see links to edit or delete projects' do
+      click_link 'log out'
+      visit '/projects'
+      expect(page).not_to have_link('edit Crowdfundingtestproject')
+      expect(page).not_to have_link('delete Crowdfundingtestproject')
+    end
 
-	end
+  end
 
-	context 'an invalid project:' do
+  context 'an invalid project:' do
 
-		it 'does not let you submit a name that is too short' do
-			visit '/projects'
-			signup_user('test@example.com', 'password123')
-			click_link 'add a project'
-			fill_in 'Name', with: 'Cr'
-			click_button 'create project'
-			expect(page).not_to have_css 'h2', text: 'Cr'
-			expect(page).to have_content 'error'
-		end
+    it 'does not let you submit a name that is too short' do
+      visit '/projects'
+      signup_user('test@example.com', 'password123')
+      click_link 'add a project'
+      fill_in 'Name', with: 'Cr'
+      click_button 'create project'
+      expect(page).not_to have_css 'h2', text: 'Cr'
+      expect(page).to have_content 'error'
+    end
 
-	end
+  end
 
 end
